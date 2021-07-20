583d9b389872152e1fb92065f4a6e4a8a8035a68
Brad Traversy <natnaelasmare777@gmail.com>
2021-07-20 05:55:47 +0300

    first commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,20 +2,21 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-    
-    return (
+    // if(true){
+    //     throw new Error('Noooo!');
+    // }
+    return (                
         <div>
-            {
-                 robots.map((user, id) => {
-                     return (
+            { //the return from the map function will be an array of cards so we need to specify it's a javascript by using {} 
+                 robots.map((user, id) => {   
+                     return (  //map function is always required to return
                         <Card 
                             key={id}
                             id={user.id}
                             name={user.name} 
                             email={user.email}
                         />
-                     );
-                    
+                     );           
                  })
             }
         </div> 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import App from './App';
+
+import App from './containers/App';
 
 ReactDOM.render(<App/> ,document.getElementById('root'));
 
